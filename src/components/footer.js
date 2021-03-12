a6c2be3109cd1dc9a9a80754bc991a9169cabed4
Kyle Caprio <55900911+capriok@users.noreply.github.com>
2021-03-11 20:16:56 -0700

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,13 +2,11 @@ import React from 'react'
 
 import '../styles/footer.scss'
 
-const Footer = () => {
-	return (
-		<footer>
-			<p>Copyright &copy; {new Date().getFullYear()} All Rights Reserved</p>
-			<p>Outlaw Performance LLC.</p>
-		</footer>
-	)
-}
+const Footer = () => (
+	<footer>
+		<p>Copyright &copy; {new Date().getFullYear()} All Rights Reserved</p>
+		<p>Outlaw Performance LLC.</p>
+	</footer>
+)
 
 export default Footer
